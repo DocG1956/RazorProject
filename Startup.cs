e6675e3e0362370bb0cafc19805fe80580051c22
DocG1956 <garybales1029@yahoo.com>
2020-09-05 15:31:14 -0500

    Implemented Mock repo

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Services;
 
 namespace RazorProject
 {
@@ -24,6 +25,7 @@ namespace RazorProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
